fdd67a5d32c8fc156f81544a6cf0305bca88e668
King-Cyrus00 <cyrus.barrow07@gmail.com>
2025-06-22 02:50:48 +0100

    navbar and footer done

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,13 +1,13 @@
 import ProductCard from "../components/ProductCard"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
 
 export default function Product () {
    return(
-     <div className="flex gap-20 ">
-      {[1,2,3].map(item=>{
-         return (
-            <ProductCard  key={item.id}  /> 
-         )
-      })}  
+     <div className="">
+
+     
+     <Navbar></Navbar>
     </div>
    )
 }
